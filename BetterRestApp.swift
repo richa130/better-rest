51db20f4b1a481089c881442bd9817e0e35760b3
Richa :) <50152225+richa130@users.noreply.github.com>
2023-01-25 14:17:26 -0800

    Add project files

---
+++ BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Richa Bavadekar on 12/29/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContentView.swift
@@ -0,0 +1,110 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Richa Bavadekar on 12/29/22.
+//
+
+import CoreML
+import SwiftUI
+
+struct ContentView: View {
+    @State private var wakeUp = defaultWakeTime
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
+    
+    var sleepTime: Date {
+        return calculateBedtime()
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section("When do you want to wake up?") {
+//                    Text("When do you want to wake up?")
+//                        .font(.headline)
+                    
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+                
+                Section("Desired amount of sleep") {
+//                    Text("Desired amount of sleep: ")
+//                        .font(.headline)
+                    
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                }
+                
+                Section("Daily coffee intake") {
+//                    Text("Daily coffee intake: ")
+//                        .font(.headline)
+                    
+//                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                    
+                    Picker("Coffee intake", selection: $coffeeAmount) {
+                        ForEach(1..<21) {
+                            //index == 1 ? Text("1 cup") : Text("\(index) cups")
+                            $0 == 1 ? Text("1 cup") : Text("\($0) cups")
+                        }
+                    }
+                }
+                
+                Section("Your ideal bedtime is...") {
+                    Text("\(sleepTime.formatted(date: .omitted, time: .shortened))")
+                }
+            }
+            .navigationTitle("BetterRest")
+//            .toolbar {
+//                Button("Calculate", action: calculateBedtime)
+//            }
+//            .alert(alertTitle, isPresented: $showingAlert) {
+//                Button("OK") { }
+//            } message: {
+//                Text(alertMessage)
+//            }
+        }
+    }
+    
+    func calculateBedtime() -> Date {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            //alertTitle = "Your ideal bedtime is..."
+//            alertMessage = "\(sleepTime.formatted(date: .omitted, time: .shortened))"
+            
+            return sleepTime
+        } catch {
+            return Date.now
+            //alertTitle = "Error"
+            //alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        
+        //showingAlert = true
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
